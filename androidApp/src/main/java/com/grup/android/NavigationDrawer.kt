839f35c22deded065bb46d726d29776b914d9a11
Alex Yu <you@example.com>
2022-12-15 21:39:48 -0800

    modified sidebar, login, and registration

--- androidApp/src/main/java/com/grup/android/NavigationDrawer.kt
+++ androidApp/src/main/java/com/grup/android/NavigationDrawer.kt
@@ -10,10 +10,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.grup.android.ui.apptheme.AppTheme
+
 
 @Composable
 fun DrawerHeader() {
